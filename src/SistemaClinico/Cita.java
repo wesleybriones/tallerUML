edf6978742327c87fa00dbe5199fd0254a450eea
abflores99 <abflores@espol.edu.ec>
2020-11-24 18:34:30 -0500

    Clase Cita

---
+++ src/SistemaClinico/Cita.java
@@ -0,0 +1,16 @@
+
+package SistemaClinico;
+import java.util.Date;
+/**
+ *
+ * @author FAMILY
+ */
+public class Cita {
+    protected Date fecha; 
+    protected boolean pagada; 
+    protected String registradoPor; 
+    
+    public void realizarPago(Pago pago){
+        
+    }
+}
