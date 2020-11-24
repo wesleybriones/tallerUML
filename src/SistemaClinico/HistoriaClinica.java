f381565e9c23ba1a81f383097625ae084ac48d75
abflores99 <abflores@espol.edu.ec>
2020-11-24 18:35:19 -0500

    Clase Clinica

---
+++ src/SistemaClinico/HistoriaClinica.java
@@ -0,0 +1,15 @@
+
+package SistemaClinico;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author FAMILY
+ */
+public class HistoriaClinica {
+    protected int nro; 
+    protected ArrayList<String> enfermedades; 
+    protected ArrayList<String> alergias; 
+    protected ArrayList<String> otros; 
+}
