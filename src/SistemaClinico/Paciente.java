aa686757dce9d3f5e25dae79685a03ae09f79bc4
wesleybriones <wesgbrio@espol.edu.ec>
2020-11-24 10:15:46 -0500

    Creacion de las clases Administrador y Paciente

---
+++ src/SistemaClinico/Paciente.java
@@ -0,0 +1,13 @@
+package SistemaClinico;
+
+/**
+ *
+ * @author wesley
+ */
+public class Paciente {
+    protected String email;
+    
+    public boolean solicitarCita(){
+        return true;
+    }
+}
